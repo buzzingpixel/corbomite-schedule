ac05696692ce927399a93929feb10f3c3c43ed54
TJ Draper <tj@buzzingpixel.com>
2019-04-08 21:14:33 -0500

    Add ActionConfigTest

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,4 +6,6 @@ ini_set('display_errors', 'On');
 ini_set('html_errors', '0');
 error_reporting(-1);
 
+define('TESTING_APP_PATH', dirname(__DIR__));
+
 require dirname(__DIR__) . '/vendor/autoload.php';
